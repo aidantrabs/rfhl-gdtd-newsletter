9e3ffd6e25fd617c2b537414905e25a8a8ba5a67
Aidan Traboulay <aidantraboulay13@gmail.com>
2026-04-14 10:48:33 -0400

    feat(data): add gm letter content

---
+++ web/src/data/gm.ts
@@ -0,0 +1,18 @@
+import type { Person } from '../types';
+import { head } from './team';
+
+type GMLetter = {
+    label: string;
+    paragraphs: string[];
+    person: Person;
+};
+
+export const gmLetter: GMLetter = {
+    label: 'A note from our GM',
+    paragraphs: [
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+        'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt.',
+    ],
+    person: head,
+};
